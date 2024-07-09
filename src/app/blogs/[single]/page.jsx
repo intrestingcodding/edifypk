9841c75650915644dc81cd92ee854da6241379eb
mubashar <intrestingcodding@gmail.com>
2024-07-09 06:52:10 +0500

    rounded

--- src/app/blogs/[single]/page.jsx
+++ src/app/blogs/[single]/page.jsx
@@ -5,7 +5,9 @@ import Sidebar from '@/components/Sidebar'
 const page = () => {
     return (
         <div className='max-w-6xl mx-auto px-4'>
-            <div className="relative">
+            <div className="relative rounded-2xl overflow-hidden">
+
+
                 <div className="absolute bottom-0 left-0 grid gap-3 w-full bg-gradient-to-t from-black p-5 pt-28 ">
                     <h1 className='text-3xl text-white '>Study in France The Ultimate Guide for Pakistani Students in 2024</h1>
                     <div className="flex gap-5">
@@ -23,10 +25,14 @@ const page = () => {
                         </div>
                     </div>
                 </div>
-                <img src="https://img.freepik.com/free-photo/group-young-students-front-school-building_23-2148199272.jpg?ga=GA1.1.633191682.1717133773&semt=ais_user" alt="" className='w-full object-cover max-h-[70vh]' />
+
+                <div className="max-h-[70vh] ">
+                    <img src="https://img.freepik.com/free-photo/group-young-students-front-school-building_23-2148199272.jpg?ga=GA1.1.633191682.1717133773&semt=ais_user" alt="" className='w-full object-cover' />
+                </div>
+
             </div>
 
-            <div className="grid grid-cols-4 max-w-6xl mx-auto gap-10 py-16 px-4">
+            <div className="grid grid-cols-4 max-w-6xl mx-auto gap-10 py-16 px-4 rounded-2xl overflow-hidden">
                 <div className="col-span-3">
                     <img src="https://img.freepik.com/free-photo/group-young-students-front-school-building_23-2148199272.jpg?ga=GA1.1.633191682.1717133773&semt=ais_user" alt="" className='w-full' />
                 </div>
